8feac92f0df3549cb4cb57853a16f1fdf0058c8a
SajjadPourali <sajjad@pourali.com>
2023-09-22 13:35:58 -0400

    Implement thiserror for other components

--- agent/src/error.rs
+++ agent/src/error.rs
@@ -4,9 +4,9 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum AgentError {
     #[error("IO Error: {0}")]
-    IoError(std::io::Error),
+    IoError(#[from] std::io::Error),
     #[error("Network Error: {0}")]
-    NetworkError(NetworkError),
+    NetworkError(#[from] NetworkError),
     #[error("Error: argument {0} is required")]
     RequiredValue(&'static str),
     #[error("Error: illegal character\nTry 'narrowlink-agent --help' for more information")]
@@ -25,17 +25,16 @@ pub enum AgentError {
     InvalidConfig,
     #[error("Unable To Resolve")]
     UnableToResolve,
-    // Invalid(&'static str),
 }
 
-impl From<std::io::Error> for AgentError {
-    fn from(err: std::io::Error) -> Self {
-        Self::IoError(err)
-    }
-}
+// impl From<std::io::Error> for AgentError {
+//     fn from(err: std::io::Error) -> Self {
+//         Self::IoError(err)
+//     }
+// }
 
-impl From<NetworkError> for AgentError {
-    fn from(err: NetworkError) -> Self {
-        Self::NetworkError(err)
-    }
-}
+// impl From<NetworkError> for AgentError {
+//     fn from(err: NetworkError) -> Self {
+//         Self::NetworkError(err)
+//     }
+// }
