8d6b1f2fdc75311a8a1441743a7ee22ba1551010
SajjadPourali <sajjad@pourali.com>
2023-10-02 23:18:07 -0400

    Add timeout for udpstream in tun

--- client/src/tun.rs
+++ client/src/tun.rs
@@ -2,6 +2,7 @@ use std::{
     collections::{hash_map::Entry, HashMap},
     net::{IpAddr, SocketAddr},
     pin::Pin,
+    time::SystemTime,
 };
 
 use futures_util::{FutureExt, SinkExt, StreamExt};
@@ -15,6 +16,7 @@ use tracing::warn;
 
 use crate::error::ClientError;
 
+const UDP_TIMEOUT: u64 = 5;
 const MTU: usize = 1500;
 
 pub enum TunStream {
@@ -230,32 +232,43 @@ pub struct TunUdpListener {
     inner: Box<netstack_lwip::UdpSocket>,
     packet_sender: mpsc::Sender<Vec<u8>>,
     streams: HashMap<(SocketAddr, SocketAddr), mpsc::Sender<Vec<u8>>>,
+    timout_rx: mpsc::Receiver<(SocketAddr, SocketAddr)>,
+    timout_tx: mpsc::Sender<(SocketAddr, SocketAddr)>,
 }
 
 impl TunUdpListener {
     pub fn new(inner: Box<netstack_lwip::UdpSocket>, packet_sender: mpsc::Sender<Vec<u8>>) -> Self {
+        let (timout_tx, timout_rx) = mpsc::channel::<(SocketAddr, SocketAddr)>(10);
         Self {
             inner,
             packet_sender,
             streams: HashMap::new(),
+            timout_rx,
+            timout_tx,
         }
     }
     pub async fn accept(&mut self) -> TunUdpStream {
-        while let Some((data, src_addr, dst_addr)) = self.inner.next().await {
-            match self.streams.entry((src_addr, dst_addr)) {
-                Entry::Occupied(stream) => {
-                    stream.get().send(data).await.unwrap();
+        loop {
+            tokio::select! {
+                Some((data, src_addr, dst_addr)) = self.inner.next() =>{
+                    match self.streams.entry((src_addr, dst_addr)) {
+                        Entry::Occupied(stream) => {
+                            stream.get().send(data).await.unwrap();
+                        }
+                        Entry::Vacant(v) => {
+                            let stream = TunUdpStream::new(src_addr, dst_addr, self.packet_sender.clone(),self.timout_tx.clone());
+                            let sender = stream.sender();
+                            sender.send(data).await.unwrap();
+                            v.insert(sender);
+                            return stream;
+                        }
+                    }
                 }
-                Entry::Vacant(v) => {
-                    let stream = TunUdpStream::new(src_addr, dst_addr, self.packet_sender.clone());
-                    let sender = stream.sender();
-                    sender.send(data).await.unwrap();
-                    v.insert(sender);
-                    return stream;
+                Some((src_addr, dst_addr)) = self.timout_rx.recv() =>{
+                    self.streams.remove(&(src_addr, dst_addr));
                 }
             }
         }
-        todo!()
     }
 }
 
@@ -265,6 +278,10 @@ pub struct TunUdpStream {
     rx: mpsc::Receiver<Vec<u8>>,
     tx: mpsc::Sender<Vec<u8>>,
     packet_sender: mpsc::Sender<Vec<u8>>,
+    last_seen: SystemTime,
+    timout_checker: tokio::time::Interval,
+    timout_tx: mpsc::Sender<(SocketAddr, SocketAddr)>,
+    timeout: bool,
 }
 
 impl TunUdpStream {
@@ -272,6 +289,7 @@ impl TunUdpStream {
         src_addr: SocketAddr,
         dst_addr: SocketAddr,
         packet_sender: mpsc::Sender<Vec<u8>>,
+        timout_tx: mpsc::Sender<(SocketAddr, SocketAddr)>,
     ) -> Self {
         let (tx, rx) = mpsc::channel(10);
         Self {
@@ -280,6 +298,10 @@ impl TunUdpStream {
             rx,
             tx,
             packet_sender,
+            last_seen: SystemTime::now(),
+            timout_checker: tokio::time::interval(tokio::time::Duration::from_secs(UDP_TIMEOUT)),
+            timout_tx,
+            timeout: false,
         }
     }
     pub fn sender(&self) -> mpsc::Sender<Vec<u8>> {
@@ -299,23 +321,52 @@ impl AsyncRead for TunUdpStream {
         cx: &mut std::task::Context<'_>,
         buf: &mut tokio::io::ReadBuf<'_>,
     ) -> std::task::Poll<std::io::Result<()>> {
-        match self.rx.poll_recv(cx) {
-            std::task::Poll::Ready(Some(v)) => {
-                buf.put_slice(&v);
-                std::task::Poll::Ready(Ok(()))
+        loop {
+            if self.timeout {
+                return match Box::pin(self.timout_tx.send((self.src_addr, self.dst_addr)))
+                    .poll_unpin(cx)
+                {
+                    std::task::Poll::Ready(_) => std::task::Poll::Ready(Err(std::io::Error::new(
+                        std::io::ErrorKind::TimedOut,
+                        "Timed out",
+                    ))),
+                    std::task::Poll::Pending => std::task::Poll::Pending,
+                };
+            };
+            match self.rx.poll_recv(cx) {
+                std::task::Poll::Ready(Some(v)) => {
+                    buf.put_slice(&v);
+                    self.last_seen = SystemTime::now();
+                    return std::task::Poll::Ready(Ok(()));
+                }
+                std::task::Poll::Ready(None) => {
+                    return std::task::Poll::Ready(Err(std::io::Error::new(
+                        std::io::ErrorKind::BrokenPipe,
+                        "Broken Pipe",
+                    )))
+                }
+                std::task::Poll::Pending => match self.timout_checker.poll_tick(cx) {
+                    std::task::Poll::Ready(_) => {
+                        if SystemTime::now()
+                            .duration_since(self.last_seen)
+                            .ok()
+                            .filter(|d| d.as_secs() > UDP_TIMEOUT)
+                            .is_some()
+                        {
+                            self.timeout = true;
+                        }
+                        continue;
+                    }
+                    _ => return std::task::Poll::Pending,
+                },
             }
-            std::task::Poll::Ready(None) => std::task::Poll::Ready(Err(std::io::Error::new(
-                std::io::ErrorKind::BrokenPipe,
-                "Broken Pipe",
-            ))),
-            std::task::Poll::Pending => std::task::Poll::Pending,
         }
     }
 }
 
 impl AsyncWrite for TunUdpStream {
     fn poll_write(
-        self: Pin<&mut Self>,
+        mut self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
         buf: &[u8],
     ) -> std::task::Poll<Result<usize, std::io::Error>> {
@@ -344,6 +395,7 @@ impl AsyncWrite for TunUdpStream {
             self.src_addr.port(),
         );
         let len = packet.size(payload.len());
+        self.last_seen = SystemTime::now();
         packet.write(&mut cursor, payload).unwrap();
         // buffer.splice(0..0, vec![0x00, 0x00, 0x00, 0x01]);
         match Box::pin(self.packet_sender.send(buffer[..len].to_vec())).poll_unpin(cx) {
