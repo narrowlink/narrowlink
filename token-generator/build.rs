57baa7769869ce85f0a280a7761da870f41ca82c
SajjadPourali <sajjad@pourali.com>
2023-11-23 18:36:48 -0500

    Change nightly versioning from timestamp to latest git hash

--- token-generator/build.rs
+++ token-generator/build.rs
@@ -1,12 +1,16 @@
-use std::time::SystemTime;
+use std::process::Command;
 
 fn main() {
-    if let (Some(val), Ok(ts)) = (
+    if let (Some(val), Some(hash)) = (
         std::env::var("CARGO_PKG_VERSION")
             .ok()
             .filter(|v| v.ends_with("-git")),
-        SystemTime::now().duration_since(SystemTime::UNIX_EPOCH),
+        Command::new("git")
+            .args(&["rev-parse", "--short", "HEAD"])
+            .output()
+            .ok()
+            .and_then(|output| String::from_utf8(output.stdout).ok()),
     ) {
-        println!("cargo:rustc-env=CARGO_PKG_VERSION={}-{}", val, ts.as_secs());
+        println!("cargo:rustc-env=CARGO_PKG_VERSION={}-{}", val, hash);
     }
 }
